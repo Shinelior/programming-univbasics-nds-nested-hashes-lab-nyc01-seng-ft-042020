1e4eccd6cd825f98971036e9d778994738b4d6c5
lior Shine <shinelior@gmail.com>
2020-03-22 19:53:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -34,8 +34,8 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  if row >= 0
-  aoh[row] = { :name => new_value }
+  
+  aoh[row][:key] = "
 else
   row += 1
 end
